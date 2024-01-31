a73f82abe4650eac7a00c40c88c04169c406a1d3
Joel Gonzalez <754537@student.dusd.net>
2024-01-31 13:06:26 -0800

    rotation y no se que

---
+++ scriptingProjectP5Gonzalez/Assets/Scripts/Translate.cs
@@ -0,0 +1,23 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translate : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+    // Start is called before the first frame update
+   
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetKey (KeyCode.DownArrow))
+                transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+    }
+}
