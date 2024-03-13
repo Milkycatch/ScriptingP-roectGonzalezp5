695c606c36f7879553e125cde4bbf1c0f207bca8
Joel Gonzalez <754537@student.dusd.net>
2024-03-13 14:56:54 -0700

    getAxis

--- scriptingProjectP5Gonzalez/Assets/Scripts/AxisExample.cs
+++ scriptingProjectP5Gonzalez/Assets/Scripts/AxisExample.cs
@@ -1,22 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class AxisExample : MonoBehaviour
-{ public float 
-        public TextAlignment text private void OnAnimatorIK(int layerIndex)
-    {
-        
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{
+    public float range;
+    public Text textOutput;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float xPos = h * range;
+
+        transform.position = new Vector3(xPos, 2f, 0);
+        textOutput.text = "Value Returned: " + h.ToString("F2");
     }
 }
